21265b899d5b3c4fa76c76d8aab2c658bd479adf
Laura Perez <lauraperez.code@gmail.com>
2015-09-11 10:14:12 -0300

    Create Nodo.java

---
+++ src/estructuraABB/Nodo.java
@@ -0,0 +1,24 @@
+package estructuraABB;
+
+public class Nodo {
+
+	private int dato;
+	public Nodo nodoDer;
+	public Nodo nodoIzq;
+
+	public Nodo() {
+
+	}
+
+	public Nodo(int d) {
+		this.dato = d;
+	}
+
+	public int getDato() {
+		return dato;
+	}
+
+	public void setDato(int dato) {
+		this.dato = dato;
+	}
+}
